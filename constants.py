d00507c4cd8afa995063199c937a1083e845103b
Romanos Grapsas <romilgrap@gmail.com>
2026-08-02 00:43:21 +0300

    extract constants

---
+++ constants.py
@@ -0,0 +1,23 @@
+ROWS = {
+    'first': 0,
+    'second': 1,
+    'third': 2,
+    'fourth': 3,
+    'fifth': 4,
+    'sixth': 5,
+    'seventh': 6,
+    'eighth': 7,
+}
+VEGETABLE_ROWS = [
+    ROWS['first'],
+    ROWS['second'],
+    ROWS['seventh'],
+    ROWS['eighth'],
+]
+PUMPKIN_COLUMNS = [
+    0,
+    1,
+    4,
+    5,
+]
+WORLD_SIZE = get_world_size()
