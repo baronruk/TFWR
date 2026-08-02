fbce315a11f9177ad27471cacc3667d81c8368e8
Romanos Grapsas <romilgrap@gmail.com>
2026-08-02 20:28:02 +0300

    extract farm_mixed

---
+++ mixed.py
@@ -0,0 +1,40 @@
+from constants import HAY_ROWS, PUMPKIN_COLUMNS, VEGETABLE_ROWS, WOOD_ROWS, WORLD_SIZE
+from helpers import do_harvest, hydrate
+from utilities import is_even
+
+
+def farm_mixed():
+    for i in range(WORLD_SIZE):
+        current_row = get_pos_y()
+        current_column = get_pos_x()
+
+        # carrots/pumkings
+        if current_row in VEGETABLE_ROWS:
+            do_harvest()
+            if get_ground_type() == Grounds.Grassland:
+                till()
+            if current_column in PUMPKIN_COLUMNS:
+                hydrate(0.5)
+                plant(Entities.Pumpkin)
+            else:
+                plant(Entities.Carrot)
+
+        # hay
+        if current_row in HAY_ROWS:
+            do_harvest()
+            plant(Entities.Grass)
+
+        # wood
+        if current_row in WOOD_ROWS:
+            do_harvest()
+
+            if is_even(get_pos_x()):
+                plant(Entities.Bush)
+            else:
+                # hydrate(0.75)
+                plant(Entities.Tree)
+
+        if current_row == (WORLD_SIZE - 1):
+            move(East)
+
+        move(North)
